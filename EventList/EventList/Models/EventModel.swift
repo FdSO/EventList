02f164c14dde13396b20d49c917e8e93b0475ea0
Filipe Oliveira <fdso@outlook.com>
2020-08-29 08:28:52 -0400

    improve(Project): Frameworks and Model layer
    [ADD] - RxSwift and others frameworks with Swift Package Manager
    [ADD] - Model layer with Codable used in REST API
    
    #close

---
+++ EventList/EventList/Models/EventModel.swift
@@ -0,0 +1,67 @@
+//
+//  EventModel.swift
+//  EventList
+//
+//  Created by Filipe Oliveira on 29/08/20.
+//  Copyright © 2020 FdSO. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+struct EventModel: Decodable {
+    
+    struct PeopleModel: Decodable {
+        
+        let id: String?
+        let eventId: String?
+        let name: String?
+        let picture: String?
+    }
+    
+    struct CouponModel: Decodable {
+        
+        let id: String?
+        let eventId: String?
+        let discount: Int?
+    }
+    
+    let id: String?
+    private let latitude: CLLocationDegrees
+    private let longitude: CLLocationDegrees
+    let date: Date?
+    let desc: String?
+    let imageUrl: URL?
+    let price: Decimal?
+    let title: String?
+    let peoples: [PeopleModel]?
+    let coupons: [CouponModel]?
+
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case latitude
+        case longitude
+        case date
+        case desc = "description"
+        case imageUrl = "image"
+        case price
+        case title
+        case coupons = "cupons"
+        case peoples = "people"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        
+        id = try? values.decode(String.self, forKey: .id)
+        latitude = try values.decodeIfPresent(CLLocationDegrees.self, forKey: .latitude) ?? .zero
+        longitude = try values.decodeIfPresent(CLLocationDegrees.self, forKey: .longitude) ?? .zero
+        date = try? values.decode(Date.self, forKey: .date)
+        desc = try? values.decode(String.self, forKey: .desc)
+        imageUrl = try? values.decode(URL.self, forKey: .imageUrl)
+        price = try? values.decode(Decimal.self, forKey: .price)
+        title = try? values.decode(String.self, forKey: .title)
+        peoples = try? values.decode([PeopleModel].self, forKey: .peoples)
+        coupons = try? values.decode([CouponModel].self, forKey: .coupons)
+    }
+}
---
+++ EventList/EventList/Models/InterestedModel.swift
@@ -0,0 +1,16 @@
+//
+//  InterestedModel.swift
+//  EventList
+//
+//  Created by Filipe Oliveira on 29/08/20.
+//  Copyright © 2020 FdSO. All rights reserved.
+//
+
+import Foundation
+
+struct InterestedModel: Codable {
+    
+    let eventId: String
+    let name: String
+    let email: String
+}
